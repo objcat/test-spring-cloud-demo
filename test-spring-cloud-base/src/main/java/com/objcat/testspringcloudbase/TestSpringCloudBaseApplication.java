ef75c7ce7baf58154abbb2f366df9b13d132f536
张祎 <objcat@163.com>
2018-12-14 17:47:43 +0800

    重构项目

---
+++ test-spring-cloud-base/src/main/java/com/objcat/testspringcloudbase/TestSpringCloudBaseApplication.java
@@ -0,0 +1,14 @@
+package com.objcat.testspringcloudbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringCloudBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringCloudBaseApplication.class, args);
+    }
+
+}
+
