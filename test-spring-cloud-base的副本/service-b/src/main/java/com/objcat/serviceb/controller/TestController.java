30d4ba569c5f655333ff8d3a234d642b8d46e183
张祎 <objcat@163.com>
2018-12-25 13:46:20 +0800

    提交

---
+++ test-spring-cloud-base的副本/service-b/src/main/java/com/objcat/serviceb/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.objcat.serviceb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Service;
+
+/**
+ * description: 服务B <br>
+ * date: 2018/12/14 下午5:37 <br>
+ * author: objcat <br>
+ * version: 1.0 <br>
+ */
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private ServiceAFeignClient serviceAFeignClient;
+
+    @RequestMapping("/call")
+    String call() {
+        return "b to a 访问结果 ----- " + serviceAFeignClient.hello();
+    }
+}
---
+++ test-spring-cloud-base的副本/service-eureka/src/test/java/com/objcat/serviceeureka/ServiceEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.objcat.serviceeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
